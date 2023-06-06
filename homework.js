f10973f19c9db4a241a32310c305c3407a82353b
sabrinaK <sabrinapsouza@hotmail.com>
2023-06-06 13:38:02 -0400

    attributs

---
+++ homework.js
@@ -0,0 +1,21 @@
+//Hamster 
+
+const person = {
+  owner: "",
+  name: "pine",
+  price: $15
+};
+
+class hamister{
+    wheelRun() {
+        console.log("squeak squeak")
+}
+
+eatFood(){
+    console.log("nibble nibble")
+}
+getPrice(){
+    return price 
+}
+}
+
